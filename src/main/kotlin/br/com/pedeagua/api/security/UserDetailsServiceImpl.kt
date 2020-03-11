f7a35e8cbb373df1633d26d523912ad09ebbb4c3
arthur.frade.araujo <arthur.frade.araujo@concrete.com.br>
2020-03-11 20:05:18 -0300

    Including transaction

--- src/main/kotlin/br/com/pedeagua/api/security/UserDetailsServiceImpl.kt
+++ src/main/kotlin/br/com/pedeagua/api/security/UserDetailsServiceImpl.kt
@@ -2,12 +2,15 @@ package br.com.pedeagua.api.security
 
 import br.com.pedeagua.api.dao.Users
 import br.com.pedeagua.api.entity.User
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 
 class UserDetailsServiceImpl : UserDetailsService {
     override fun loadUserByUsername(username: String?): UserDetails {
-        val user = User.find { Users.username eq username!! }.firstOrNull()
+        val user = transaction {
+            User.find { Users.username eq username!! }.firstOrNull()
+        }
 
         return UserDetailsImpl(user!!)
     }
