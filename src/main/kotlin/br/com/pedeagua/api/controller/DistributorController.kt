1b4a092ad8b3178fee26246b16a4e957e5849e7b
arthur.frade.araujo <arthur.frade.araujo@concrete.com.br>
2020-03-11 14:11:15 -0300

    Create distributor function

--- src/main/kotlin/br/com/pedeagua/api/controller/DistributorController.kt
+++ src/main/kotlin/br/com/pedeagua/api/controller/DistributorController.kt
@@ -1,6 +1,7 @@
 package br.com.pedeagua.api.controller
 
 import br.com.pedeagua.api.entity.dto.DistributorRequest
+import br.com.pedeagua.api.service.DistributorService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("v1/distributor")
-class DistributorController {
+@RequestMapping("v1/distributors")
+class DistributorController(val distributorService: DistributorService) {
 
     @PostMapping
-    fun create(@RequestBody distributor: DistributorRequest): ResponseEntity<Any> =
-            ResponseEntity.status(HttpStatus.CREATED).build()
+    fun create(@RequestBody distributorDto: DistributorRequest): ResponseEntity<Any> {
+        distributorService.create(distributorDto)
+        return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
 
 }
--- src/main/kotlin/br/com/pedeagua/api/service/DistributorService.kt
+++ src/main/kotlin/br/com/pedeagua/api/service/DistributorService.kt
@@ -1,7 +1,45 @@
 package br.com.pedeagua.api.service
 
+import br.com.pedeagua.api.dao.Phones
+import br.com.pedeagua.api.entity.Address
+import br.com.pedeagua.api.entity.Distributor
+import br.com.pedeagua.api.entity.User
+import br.com.pedeagua.api.entity.dto.DistributorRequest
+import org.jetbrains.exposed.sql.batchInsert
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Service
 
 @Service
 class DistributorService {
+
+    fun create(distributorDto: DistributorRequest) {
+        transaction {
+            val persistedUser = User.new {
+                username = distributorDto.mail
+                password = distributorDto.password
+            }
+
+            val persistedAddress = Address.new {
+                street = distributorDto.address.street
+                number = distributorDto.address.number
+                neighbourhood = distributorDto.address.neighbourhood
+                zipCode = distributorDto.address.zipCode
+                complement = distributorDto.address.complement
+            }
+
+            Phones.batchInsert(distributorDto.phones) { phoneDto ->
+                this[Phones.ddd] = phoneDto.ddd
+                this[Phones.number] = phoneDto.number
+                this[Phones.user] = persistedUser.id
+            }
+
+            Distributor.new {
+                name = distributorDto.name
+                email = distributorDto.mail
+                cnpj = distributorDto.cnpj
+                user = persistedUser
+                address = persistedAddress
+            }
+        }
+    }
 }
