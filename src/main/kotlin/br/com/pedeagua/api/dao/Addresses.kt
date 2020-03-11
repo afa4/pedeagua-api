272f6d4041ef4b52a1a4d840043b49418623b3dd
arthur.frade.araujo <arthur.frade.araujo@concrete.com.br>
2020-03-11 14:10:17 -0300

    Fixing table definition

--- src/main/kotlin/br/com/pedeagua/api/dao/Addresses.kt
+++ src/main/kotlin/br/com/pedeagua/api/dao/Addresses.kt
@@ -3,9 +3,9 @@ package br.com.pedeagua.api.dao
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object Addresses : LongIdTable() {
-    val street = Distributors.varchar("street", 100)
-    val number = Distributors.varchar("number", 50)
-    val zipCode = Distributors.varchar("zip_code", 50)
-    val neighbourhood = Distributors.varchar("neighbourhood", 100)
-    val complement = Distributors.varchar("complement", 100).nullable()
+    val street = varchar("street", 100)
+    val number = varchar("number", 50)
+    val zipCode = varchar("zip_code", 50)
+    val neighbourhood = varchar("neighbourhood", 100)
+    val complement =  varchar("complement", 100).nullable()
 }
